33ce29f5c952b289430c54d90fbd62aee3ca4ead
Sanket Khilwani <sanket.khilwani@amla.io>
2019-04-25 18:14:59 +0530

    Sample project

---
+++ utilities/driver1/Browser.java
@@ -0,0 +1,12 @@
+package driver1;
+
+public class Browser {
+
+	public static void maximizeBrowser()
+	{
+		Invoke.webDriver.manage().window().maximize();
+		
+	}// End of maximizeBrowser
+	
+		
+}//End of BrowserCapabilities
